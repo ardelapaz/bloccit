5d656157fb0d2bead48880a1ea6c8ac05790dce7
Andrew Delapaz <ardelapaz@yahoo.com>
2018-04-26 21:13:41 -0400

    Intro to CRUD Checkpoint finished

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,19 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts.each_with_index do |post, index|
+      if index % 5 == 0
+        post.title = "Spam"
+      end
+    end
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def edit
