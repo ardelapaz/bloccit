90e0ec800ff042bfc043571a5c5114b14fe55fd2
Andrew Delapaz <ardelapaz@yahoo.com>
2018-05-06 00:24:03 -0400

    Pre assignment Topic work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,17 @@
 require 'random_data'
 
 # Create Posts
+15.times do
+  Topic.create!(
+    name:         RandomData.random_sentence,
+    description:  RandomData.random_paragraph
+  )
+end
+topics = Topic.all
+
 50.times do
   Post.create!(
+    topic:  topics.sample,
     title:  RandomData.random_sentence,
     body:   RandomData.random_paragraph
   )
@@ -30,6 +39,7 @@ Post.find_or_create_by(title: "title", body: "body")
 Comment.find_or_create_by(post:Post.find_or_create_by(title: "title", body: "body"), body: "commentBody")
 
 puts "Seed finished"
+puts "#{Topic.count} topics created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
-puts "#{Question.count}" questions created"
+puts "#{Question.count} questions created"
