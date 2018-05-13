0dfa9724bd8303ff56cab12ecd913ea90ebb2c10
Andrew Delapaz <ardelapaz@yahoo.com>
2018-05-12 22:09:00 -0400

    Progress on checkpoint 27

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
     has_many :posts, dependent: :destroy
 
 before_save { self.email = email.downcase if email.present? }
-before_save { :format_name }
+before_save { self.role ||= :member }
 #before_save :format_name 
 
 validates :name, length: { minimum: 1, maximum: 100 }, presence: true
@@ -14,6 +14,10 @@ validates :email,
             length: { minimum: 3, maximum: 254 }
 
    has_secure_password
+
+   
+   enum role: [:member, :admin]
+
 
 
     def format_name
